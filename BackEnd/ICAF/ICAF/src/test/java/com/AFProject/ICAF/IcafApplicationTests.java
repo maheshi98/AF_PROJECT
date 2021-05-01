67be72fc9cd327451aa44b078e77b4adcff3dd0e
Maheshi Purnima <mashipurnima1998@gmail.com>
2021-05-01 17:23:53 +0530

    backend project added

---
+++ BackEnd/ICAF/ICAF/src/test/java/com/AFProject/ICAF/IcafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AFProject.ICAF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
