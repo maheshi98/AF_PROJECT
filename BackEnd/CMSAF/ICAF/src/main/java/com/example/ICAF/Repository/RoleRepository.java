3dbfb40a26edb59e26bc473896fc19e950aecb6f
Perusha <69392455+Perusha@users.noreply.github.com>
2021-05-02 21:11:49 +0530

    Delete BackEnd/CMSAF directory

--- BackEnd/CMSAF/ICAF/src/main/java/com/example/ICAF/Repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ICAF.Repository;
-
-
-import com.example.ICAF.Model.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role,String> {
-
-}
--- BackEnd/CMSAF/cms/src/main/java/com/backend/cms/Repository/sampleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.backend.cms.Repository;
-
-import com.backend.cms.Model.sampleMod;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface sampleRepo extends MongoRepository<sampleMod,String> {
-
-
-}
