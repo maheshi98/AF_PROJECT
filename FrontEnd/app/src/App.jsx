929fe10bfbf093a0402c272585e77bb2497a5521
Dulya Perera <dulyakemali@gmail.com>
2021-05-29 18:25:58 +0530

    Remove not essential code

--- FrontEnd/app/src/App.jsx
+++ FrontEnd/app/src/App.jsx
@@ -8,16 +8,8 @@ export default class App extends Component {
     constructor(props) {
         super(props)
         this.state = {}
-        this.handleEvent = this.handleEvent.bind(this)
     }
-
-    componentDidMount() { }
-    componentDidUpdate(prevProps, prevState, snapshot) { if (prevState.name !== this.state.name) { this.handler() } }
-    componentWillUnmount() { }
-    // Prototype methods, Bind in Constructor (ES2015)
-    handleEvent() { }
-    // Class Properties (Stage 3 Proposal)
-    // handler = () => { this.setState() }
+    
     render() {
         return (
             <div>
