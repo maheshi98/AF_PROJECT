6df00abdf0eddcbb1a3d2e39025cc26ddeba3a6f
Dulya Perera <dulyakemali@gmail.com>
2021-05-29 18:28:49 +0530

    Add card to topics

--- FrontEnd/app/src/Components/Home/PaperTopicsSection/PaperTopics.jsx
+++ FrontEnd/app/src/Components/Home/PaperTopicsSection/PaperTopics.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardGroup, Card } from 'react-bootstrap';
 import './PaperTopicsStyles.css';
 
 export default class MainSection extends Component {
@@ -15,12 +16,49 @@ export default class MainSection extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Call for Papers</h1>
-                <p>Dulya Perera</p>
-                <p>Dulya Perera</p>
-                <p>Dulya Perera</p>
-                <p>Dulya Perera</p>
+            <div style={{margin: 20}}>
+                <CardGroup>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+      </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="../../../Assets/logo.png" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This card has even longer content than the first to
+                                show that equal height action.
+      </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
             </div>
         );
     }
