4e3d1db4ec67f060e9c15b2bcbc1755bab4f8f1f
Dulya Perera <dulyakemali@gmail.com>
2021-06-10 23:13:52 +0530

    Add about section

--- FrontEnd/app/src/Components/Home/AboutSection/AboutSection.jsx
+++ FrontEnd/app/src/Components/Home/AboutSection/AboutSection.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './AboutSectionStyles.css';
+import logo from 'url:~/src/Assets/new.png';
 
 export default class MainSection extends Component {
     constructor(props) {
@@ -16,21 +17,30 @@ export default class MainSection extends Component {
     render() {
         return (
             <div className="mainContainer">
-                <h1>About ICAF 2021</h1>
-                <p>The 3rd International conference on advancements in computing -2021 (ICAC2021)
-                is organized by the Faculty of Computing of the Sri Lanka Institute of Information
-                Technology (SLIIT) as an open forum for academics along with industry professionals
-                to present the latest findings and research output and practical deployments in the
-                Computer Science and Information Technology domains. Primary objective of the ICAC
-                is to uplift the research culture and the quality of research done by Sri Lankan
-                researchers. This conference will create a platform for national and international
-                researchers to showcase their research output, networking opportunities to discuss
-                innovative ideas, and initiate collaborative work. ICAC 2019 and ICAC 2020 were
-                successfully conducted with a technical co-sponsorship by IEEE Sri Lanka Section
-                and all publications are available in IEEE Xplore digital library</p>
-
-                <h2>Venue</h2>
-                <h2>Date</h2>
+                <div id='about'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-xs-12 col-md-6'>
+                                <img src={logo} className='img-responsive' alt='' />{' '}
+                            </div>
+                            <div className='col-xs-12 col-md-6'>
+                                <div className='about-text'>
+                                    <h2>About Conference</h2>
+                                    <p>DevLearn will provide you with the answers to today’s toughest 
+                                        challenges, while also giving you the guidance to navigate the 
+                                        uncertain times ahead. You’ll find passionate, like-minded 
+                                        professionals who are leading their organizations’ use of 
+                                        learning technologies. The comprehensive program includes tools, 
+                                        technologies, ideas, strategies, and best practices to ensure your 
+                                        success. Whether you’re new to the field or a seasoned expert, 
+                                        DevLearn is the place to engage with your professional community 
+                                        and explore how technology can support learning and development. 
+                                        Attendees’ roles include:</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
