c995da4fdf4949631d24fbe488ffff5c948b443e
Dulya Perera <dulyakemali@gmail.com>
2021-06-11 17:02:00 +0530

    About section update

--- FrontEnd/app/src/Components/Home/AboutSection/AboutSection.jsx
+++ FrontEnd/app/src/Components/Home/AboutSection/AboutSection.jsx
@@ -26,16 +26,18 @@ export default class MainSection extends Component {
                             <div className='col-xs-12 col-md-6'>
                                 <div className='about-text'>
                                     <h2>About Conference</h2>
-                                    <p>DevLearn will provide you with the answers to today’s toughest 
-                                        challenges, while also giving you the guidance to navigate the 
-                                        uncertain times ahead. You’ll find passionate, like-minded 
-                                        professionals who are leading their organizations’ use of 
-                                        learning technologies. The comprehensive program includes tools, 
-                                        technologies, ideas, strategies, and best practices to ensure your 
-                                        success. Whether you’re new to the field or a seasoned expert, 
-                                        DevLearn is the place to engage with your professional community 
-                                        and explore how technology can support learning and development. 
-                                        Attendees’ roles include:</p>
+                                    <p>ICAF is the most excited conference held around the world. 
+                                        This is the 1st time that the conference comes as a virtual
+                                        online conference which leads to participate all of interested people. 
+                                        It will provide you with the answers to today’s toughest
+                                        challenges, while also giving you the guidance to navigate the
+                                        uncertain times ahead. You’ll find passionate, like-minded
+                                        professionals who are leading their organizations’ use of
+                                        learning technologies. The comprehensive program includes tools,
+                                        technologies, ideas, strategies, and best practices to ensure your
+                                        success. Whether you’re new to the field or a seasoned expert,
+                                        DevLearn is the place to engage with your professional community
+                                        and explore how technology can support learning and development.</p>
                                 </div>
                             </div>
                         </div>
