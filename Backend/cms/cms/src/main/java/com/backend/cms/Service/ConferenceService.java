127da3f43e1e6a8f59993abe3a2d81bc9c11139f
Dulya Perera <dulyakemali@gmail.com>
2021-06-24 09:16:04 +0530

    Add changes

--- Backend/cms/cms/src/main/java/com/backend/cms/Service/ConferenceService.java
+++ Backend/cms/cms/src/main/java/com/backend/cms/Service/ConferenceService.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ConferenceServ {
+public class ConferenceService {
 
     @Autowired
     public ConferenceSessionRepo conferenceSessionRepo;
-
 }
 
