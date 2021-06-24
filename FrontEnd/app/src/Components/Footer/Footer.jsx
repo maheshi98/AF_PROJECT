ca604d0c8f752cf289dfce5b7dcab62b428752fb
Dulya Perera <dulyakemali@gmail.com>
2021-06-24 20:46:54 +0530

    Footer changes

--- FrontEnd/app/src/Components/Footer/Footer.jsx
+++ FrontEnd/app/src/Components/Footer/Footer.jsx
@@ -171,9 +171,9 @@ export default class Footer extends Component {
                 <div id='footer'>
                     <div className='container text-center'>
                         <p>
-                            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-                            <a href='http://www.templatewire.com' rel='nofollow'>
-                                TemplateWire
+                            &copy; 2021 ICAF{' '}
+                            <a href='http://www.icaf.com' rel='nofollow'>
+                                ICAF
             </a>
                         </p>
                     </div>
