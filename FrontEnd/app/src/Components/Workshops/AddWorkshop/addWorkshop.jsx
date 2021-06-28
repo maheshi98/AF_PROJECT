fd5895c7fad3fc74e483352e7ca27f8165578338
AchiniIma <achiniimalka@gmail.com>
2021-06-28 17:58:57 +0530

    file upload update

--- FrontEnd/app/src/Components/Workshops/AddWorkshop/addWorkshop.jsx
+++ FrontEnd/app/src/Components/Workshops/AddWorkshop/addWorkshop.jsx
@@ -12,9 +12,19 @@ const initialState = {
      time: "",
      mobileNo: "",
      email: "",
-    // proposal: "",
-     //fileLink: "",
-     //status: "",
+     proposal: "",
+     fileLink: "",
+     status: "",
+     fileName:"",
+     fileDownloadUri:"",
+     fileType:"",
+     size:"",
+     selectedFiles: undefined,
+     currentFile: undefined,
+     progress: 0,
+     message: "",
+
+     fileInfos: [],
      submitted: false
 }
 
@@ -24,6 +34,16 @@ export default class addWorkShop extends Component{
         this.onChange = this.onChange.bind(this);
         this.state = initialState;
         this.saveWorkshop = this.saveWorkshop.bind(this);
+        this.selectFile = this.selectFile.bind(this);
+        this.upload = this.upload.bind(this);
+    }
+
+    componentDidMount() {
+      WorkshopDataService.getFiles().then((response) => {
+        this.setState({
+          fileInfos: response.data,
+        });
+      });
     }
        
         onChange(e) {
@@ -59,8 +79,51 @@ export default class addWorkShop extends Component{
         console.log(e);
       });
   }
+
+  selectFile(event) {
+    this.setState({
+      selectedFiles: event.target.files,
+    });
+  }
+
+  upload() {
+    let currentFile = this.state.selectedFiles[0];
+
+    this.setState({
+      progress: 0,
+      currentFile: currentFile,
+    });
+    WorkshopDataService.upload(currentFile, (event) => {
+      this.setState({
+        progress: Math.round((100 * event.loaded) / event.total),
+      });
+    })
+      .then((response) => {
+        this.setState({
+          message: response.data.message,
+        });
+        return WorkshopDataService.getFiles();
+      })
+      .then((files) => {
+        this.setState({
+          fileInfos: files.data,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          progress: 0,
+          message: "Could not upload the file!",
+          currentFile: undefined,
+        });
+      });
+
+      this.setState({
+        selectedFiles: undefined,
+      });
+    }
     
     render() {
+      const {selectedFiles, currentFile, progress, message, fileInfos,} = this.state;
         return (
             <div className='container'>
               <div id='createWorkshop'>
@@ -115,6 +178,34 @@ export default class addWorkShop extends Component{
                           value ={this.state.email}
                           onChange={this.onChange} />
                           </Form.Group>
+                         <Form.Group>
+        {currentFile && (
+          <div className="progress">
+            <div
+              className="progress-bar progress-bar-info progress-bar-striped"
+              role="progressbar" aria-valuenow={progress}aria-valuemin="0" aria-valuemax="100" style={{ width: progress + "%" }} >
+              {progress}%
+            </div>
+          </div>
+        )}
+        <Form.Label>Proposal</Form.Label>
+          <Form.Control 
+           type="file" 
+           onChange={this.selectFile} />
+<button className="btn btn-success" disabled={!selectedFiles} onClick={this.upload}>Upload</button>
+  <div className="alert alert-light" role="alert">{message}</div>
+<div className="card">
+          <div className="card-header">List of Files</div>
+          <ul className="list-group list-group-flush">
+            {fileInfos &&
+              fileInfos.map((file, index) => (
+                <li className="list-group-item" key={index}>
+                  <a href={file.url}>{file.name}</a>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </Form.Group>
                           <Button type="submit" style={{ backgroundColor: '#37474F', paddingRight: 10 }}>Submit</Button> {''}
                     </form>
                     </Col>
