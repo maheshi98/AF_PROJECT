7d4e01970a118eb0a29fd184eed7f368d7a86bd3
Dulya Perera <dulyakemali@gmail.com>
2021-06-29 17:47:51 +0530

    Display conference in mainpage

--- FrontEnd/app/src/BizLogic/ConferenceHandler.js
+++ FrontEnd/app/src/BizLogic/ConferenceHandler.js
@@ -8,19 +8,19 @@ import ConferenceService from '../Services/Conference';
 export const createConferenceFn = (data) => {
     console.log("ConferenceHandler :: createConferenceFn", data);
     ConferenceService.create(data)
-    .then(response => {
-        // return "Data successfully entered.";
-        alert('Data successfully entered.');
-    })
-    .catch(error => {
-        console.log(error.message);
-        alert(error.message);
-        // return error.message;
-    })
+        .then(response => {
+            // return "Data successfully entered.";
+            alert('Data successfully entered.');
+        })
+        .catch(error => {
+            console.log(error.message);
+            alert(error.message);
+            // return error.message;
+        })
 }
 
 /**
- * @description This method get all Important Dates
+ * @description This method get all Conferences
  * @param {Function} callback
  * @memberof ConferenceHandler
  */
@@ -31,12 +31,25 @@ export const getAllConferenceFn = (callback) => {
         .catch(error => callback({ error: error }));
 }
 
-// /**
-//  * @description This method get Important Date By ID
-//  * @param {String} id 
-//  * @memberof ConferenceHandler
-//  */
-// export const getImportantDateByIdFn = (id) => {
-//     console.log("ConferenceHandler :: getImportantDateByIdFn", id);
-//     return ImportantDateService.get(id).then(response => response.data);
-// }
+/**
+ * @description This method get Conference By ID
+ * @param {String} id 
+ * @memberof ConferenceHandler
+ */
+export const getConferenceByIdFn = (id) => {
+    console.log("ConferenceHandler :: getConferenceByIdFn", id);
+    return ConferenceService.get(id).then(response => response.data);
+}
+
+
+/**
+ * @description This method get Approved Conference
+ * @param {Function} callback
+ * @memberof ConferenceHandler
+ */
+export const getApprovedConferenceFn = (callback) => {
+    console.log("ConferenceHandler :: getApprovedConferenceFn", callback);
+    ConferenceService.approvedConference()
+        .then(response => callback({ data: response.data }))
+        .catch(error => callback({ error: error }));
+}
--- FrontEnd/app/src/Components/Conference/Conference.jsx
+++ FrontEnd/app/src/Components/Conference/Conference.jsx
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { CardDeck, Card, Button, CardColumns } from 'react-bootstrap';
 import './Conference.css';
-import { CardDeck, Card } from 'react-bootstrap';
 import PageTitle from '../PageTitle/PageTitle';
+import { getApprovedConferenceFn } from '../../BizLogic';
 
+const initialState = {
+    approvedConferences: []
+}
 export default class Conference extends Component {
+    constructor(props) {
+        super(props);
+        this.getAllApprovedConference = this.getAllApprovedConference.bind(this);
+        this.viewMoreDetails = this.viewMoreDetails.bind(this);
+        this.state = initialState;
+    }
+
+    componentDidMount() {
+        this.getAllApprovedConference();
+    }
+
+    /**
+    * @description This method retrieve all approved Conference
+    * @memberof ViewConference
+    */
+    getAllApprovedConference = () => {
+        const callbackFn = (result) => {
+            const { data, error } = result;
+            console.log("data", data);
+            if (data) {
+                this.setState({ approvedConferences: data });
+            }
+            if (error) {
+                console.log(error);
+            }
+        }
+        {/** Calling function to retrieve data */ }
+        getApprovedConferenceFn(callbackFn);
+    }
+
+    /**
+    * @description This method retrieve all approved Conference
+    * @memberof ViewConference
+    */
+    viewMoreDetails = (id) => {
+        this.props.history.push("/conference-details/" + id);
+    }
 
     render() {
         return (
@@ -12,37 +54,28 @@ export default class Conference extends Component {
                 <div className="container">
                     <div id='conference'>
                         <div className='container'>
-                            <div className='section-title text-center'>
+                            <h5>We are thrilled to continue our Learning Guild Certificate Programs at DevLearn. These Guild-certified two-day experiences prioritize paths to competency on core topics. </h5>
+                            {/* <div className='section-title text-center'>
                                 <h2>Conference Details</h2>
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-                                    dapibus leonec. </p>
-                            </div>
-                            <CardDeck>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Card Name</Card.Title>
-                                        <Card.Text>job </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Card Name</Card.Title>
-                                        <Card.Text>job </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Card Name</Card.Title>
-                                        <Card.Text>job </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Card Name</Card.Title>
-                                        <Card.Text>job </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </CardDeck>
+                            </div> */}
+                            <br /><br />
+                            <CardColumns>
+                                {
+                                    this.state.approvedConferences.map(
+                                        conference =>
+                                            <Card style={{ backgroundColor: "ButtonFace", left: "50%" }}>
+                                                <Card.Body>
+                                                    <Card.Title>{conference.confTopic}</Card.Title>
+                                                    <h6>{conference.confDate}</h6>
+                                                    <Card.Text>{conference.confDescription}</Card.Text>
+                                                    <div className="text-center">
+                                                        <Button variant="info" onClick={() => this.viewMoreDetails(conference.confId)}>More Details</Button>
+                                                    </div>
+                                                </Card.Body>
+                                            </Card>
+                                    )
+                                }
+                            </CardColumns>
                         </div>
                     </div>
                 </div>
