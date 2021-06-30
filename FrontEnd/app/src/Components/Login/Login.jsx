8b1021ec0e44a609a67e5459088ecb884048ba7e
Dulya Perera <dulyakemali@gmail.com>
2021-06-30 08:59:22 +0530

    Changes

--- FrontEnd/app/src/Components/Login/Login.jsx
+++ FrontEnd/app/src/Components/Login/Login.jsx
@@ -4,41 +4,41 @@ import Button from 'react-bootstrap/Button';
 import RegisterUserService from '../../Services/RegisterUserService';
 
 export default class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    
+
     this.state = {
-    currentUser : [],
-    userInvalid : [],
-  
-    email : null,
-    password : null,
-    error : null,
-    isFound : false,
-    isValid : false,
+      currentUser: [],
+      userInvalid: [],
+
+      email: null,
+      password: null,
+      error: null,
+      isFound: false,
+      isValid: false,
 
     };
   }
   handleChange = this.handleChange.bind(this);
   shoot = this.shoot.bind(this);
 
-    componentDidMount() {
-        
-    }  handleChange(event) {  
+  componentDidMount() {
+
+  } handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
-      
-  });
 
-  
-}
-shoot(event){
-  //console.log(this.state.email , this.state.password);
- /* UserService.getUserById(this.state.email).then(res => {
-    let user = res.data;
-    console.log('User => ' + JSON.stringify(user));
-    this.props.history.push("/CustomerProfile")
-});*/
+    });
+
+
+  }
+  shoot(event) {
+    //console.log(this.state.email , this.state.password);
+    /* UserService.getUserById(this.state.email).then(res => {
+       let user = res.data;
+       console.log('User => ' + JSON.stringify(user));
+       this.props.history.push("/CustomerProfile")
+   });*/
     event.preventDefault();
 
     RegisterUserService.getUserByIdOptional(this.state.email).then(res => {
@@ -47,104 +47,103 @@ shoot(event){
       console.log('User => ' + JSON.stringify(user));
       //this.props.history.push("/CustomerProfile");
 
-      if(user == null){
+      if (user == null) {
         this.state.isFound = false;
         alert("Invalid Credentials!!");
         return;
       }
-      
-      if(this.state.email == user.email && this.state.password == user.password){
+
+      if (this.state.email == user.email && this.state.password == user.password) {
         this.state.isFound = true;
         window.sessionStorage.setItem("UserId", user.email);
         this.state.isValid = true;
         this.state.currentUser = user;
-        
 
 
-      } else{
+
+      } else {
 
         //window.sessionStorage.setItem("UserId", "NF");
-        this.state.isValid =  false;
+        this.state.isValid = false;
       }
 
-      
-      if(this.state.isValid == true){
-        if(user.role == "USER"){
-          window.sessionStorage.setItem("UserRole","USER");
-          this.state.isFound = true;
-          this.props.history.push('/');
-          window.sessionStorage.setItem("UserRole","USER");
 
-          if(user.roleId == "Attendee"){
-            this.state.isFound = true;
-            this.props.history.push('/');
-            window.sessionStorage.setItem("RoleType","Attendee");
+      if (this.state.isValid == true) {
+        if (user.role == "USER") {
+          window.sessionStorage.setItem("UserRole", "USER");
+          this.state.isFound = true;
+          this.props.history.push('/');
+          window.sessionStorage.setItem("UserRole", "USER");
 
-          }else if(user.roleId == "Workshop Conductor"){
+          if (user.roleId == "Attendee") {
             this.state.isFound = true;
             this.props.history.push('/');
-            window.sessionStorage.setItem("RoleType","Workshop Conductor");
-          }
-          else if(user.roleId == "Researcher"){
+            window.sessionStorage.setItem("RoleType", "Attendee");
+
+          } else if (user.roleId == "Workshop Conductor") {
             this.state.isFound = true;
             this.props.history.push('/');
-            window.sessionStorage.setItem("RoleType","Researcher");
+            window.sessionStorage.setItem("RoleType", "Workshop Conductor");
+          }
+          else if (user.roleId == "Researcher") {
+            this.state.isFound = true;
+            this.props.history.push('/');
+            window.sessionStorage.setItem("RoleType", "Researcher");
           }
         }
-        else if(user.role == "ADMIN"){
-          window.sessionStorage.setItem("UserRole","ADMIN");
+        else if (user.role == "ADMIN") {
+          window.sessionStorage.setItem("UserRole", "ADMIN");
           this.state.isFound = true;
           this.props.history.push('/');
-          window.sessionStorage.setItem("UserRole","ADMIN");
+          window.sessionStorage.setItem("UserRole", "ADMIN");
         }
-        else if(user.role == "EDITOR"){
-          window.sessionStorage.setItem("UserRole","EDITOR");
+        else if (user.role == "EDITOR") {
+          window.sessionStorage.setItem("UserRole", "EDITOR");
           this.state.isFound = true;
           this.props.history.push('/');
-          window.sessionStorage.setItem("UserRole","EDITOR");
+          window.sessionStorage.setItem("UserRole", "EDITOR");
         }
-        else if(user.role == "REVEIWER"){
-          window.sessionStorage.setItem("UserRole","REVEIWER");
+        else if (user.role == "REVEIWER") {
+          window.sessionStorage.setItem("UserRole", "REVEIWER");
           this.state.isFound = true;
           this.props.history.push('/');
-          window.sessionStorage.setItem("UserRole","REVEIWER");
+          window.sessionStorage.setItem("UserRole", "REVEIWER");
         }
-      } else if(this.state.isValid == false && this.state.isFound == true) {
+      } else if (this.state.isValid == false && this.state.isFound == true) {
         this.state.isFound = true;
         alert("Password does not match with the given email!");
         this.props.history.push('/login');
 
-      } 
-      
+      }
+
 
       //this.props.history.push("/CustomerProfile")
     });
-}
+  }
 
 
-   
 
-    render()
-     {
-        return (
-            <div className="bg-img">
+
+  render() {
+    return (
+      <div className="bg-img">
         <div className="content">
           <header>Login Form</header>
           <form action="#">
             <div className="field">
               <span className="fa fa-user" />
-              <input type = "email" required placeholder="Email or Phone" value={this.state.email}  onChange={this.handleChange} className="form-control"name="email"/>
+              <input type="email" required placeholder="Email or Phone" value={this.state.email} onChange={this.handleChange} className="form-control" name="email" />
             </div>
             <div className="field space">
               <span className="fa fa-lock" />
-              <input type = "password" value={this.state.password} onChange={this.handleChange}className="form-control" name="password" placeholder="Enter Password" required/>
+              <input type="password" value={this.state.password} onChange={this.handleChange} className="form-control" name="password" placeholder="Enter Password" required />
               <span className="show">SHOW</span>
             </div>
             <div className="pass">
               <a href="#">Forgot Password?</a>
             </div>
             <div>
-            <Button type="submit" variant="primary btn-lg" onClick={this.shoot} className="btn btn-danger btn-block" ><font size="4">Login</font></Button>
+              <Button type="submit" variant="primary btn-lg" onClick={this.shoot} className="btn btn-danger btn-block" ><font size="4">Login</font></Button>
             </div>
           </form>
           <div className="login">Or login with</div>
@@ -155,7 +154,6 @@ shoot(event){
           </div>
         </div>
       </div>
-        )
-    }
+    )
+  }
 }
-
